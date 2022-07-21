a6dc07f893c5140d20b79eca515307fde74222cc
tkdlek0501 <tkdlek0501@naver.com>
2022-07-21 21:45:52 +0900

    Member Entity 생성

---
+++ src/main/java/com/myapi/demo/entity/Member.java
@@ -0,0 +1,21 @@
+package com.myapi.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Member {
+	
+	@Id @GeneratedValue
+	@Column(name = "member_id")
+	private Long id;
+	
+	private String username;
+	
+	private int age;
+}
