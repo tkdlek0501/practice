7bb23c4ece1d9dab95651d80e15a5fd53e7c46fb
tkdlek0501 <tkdlek0501@naver.com>
2022-12-09 00:08:29 +0900

    properties 수정 - jpa db 생성 설정 안되는 문제 해결(spring.profiles 설정 주석)

--- src/main/java/com/myapi/demo/DemoApplication.java
+++ src/main/java/com/myapi/demo/DemoApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class DemoApplication { // main 함수를 run 하면  스프링부트 실행
 
 	public static void main(String[] args) {
